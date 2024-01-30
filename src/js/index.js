8f26e3d7effbf82cab46216f079d73f772c2e1e9
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-01-30 21:14:21 +0300

    Added running-line with animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '@/styles/style';
+
+const runningLineList = document.querySelectorAll('.running-line');
+
+runningLineList.forEach((runningLine) => {
+  runningLine.innerHTML += runningLine.innerHTML;
+});
